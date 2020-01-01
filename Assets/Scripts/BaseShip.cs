9eefa85e55f557fe1470627b8da6863a7b99d481
Laurie Trichet <laurie@twobulls.com>
2020-01-02 00:45:29 +1100

    add files

---
+++ Assets/Scripts/BaseShip.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(HealthSystem))]
+
+public abstract class BaseShip : MonoBehaviour
+{
+    [SerializeField] GameObject laserPrefab = null;
+    [SerializeField] GameObject explosionPrefab = null;
+
+    protected HealthSystem healthSystem = null;
+    protected SpriteRenderer spriteRenderer = null;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        healthSystem = GetComponent<HealthSystem>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        healthSystem.hasDied = OnDeath;
+    }
+
+    protected virtual void Shoot()
+    {
+        SpawnLaser();
+    }
+
+    protected void SpawnLaser()
+    {
+        Instantiate(laserPrefab, gameObject.transform.position, Quaternion.identity);
+    }
+
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        var damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (damageDealer)
+        {
+            damageDealer.Hit();
+            healthSystem.HandleHit(damageDealer);
+        }
+    }
+
+    protected virtual void OnDeath()
+    {
+        PlayExplosionAnim();
+        Destroy(gameObject);
+    }
+
+    private void PlayExplosionAnim()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity, transform.parent);
+    }
+}
