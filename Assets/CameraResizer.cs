1997da4bbd3f9fe1c60d4b3ca07c5dd03f7bc775
Laurie Trichet <laurie.trichet@gmail.com>
2020-03-09 15:39:55 +1100

    add the camera resizer script to set the play zone to be 9/16th

---
+++ Assets/CameraResizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResizer : MonoBehaviour
+{
+
+    [SerializeField] Camera targetCamera = null;
+
+    void Start()
+    {
+        float ratioGame = 9.0f/16.0f;
+        float ratioScreen = Screen.width / Screen.height;
+        float width = ratioGame * Screen.height / ratioScreen;
+        float normalizedWidth = width/Screen.width;
+        float centeredX = (1.0f - normalizedWidth) / 2.0f;
+        targetCamera.rect = new Rect(new Vector2(centeredX, 0.0f), new Vector2(normalizedWidth, 1.0f));
+    }
+
+
+}
